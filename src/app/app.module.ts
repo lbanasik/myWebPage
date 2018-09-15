7f7f603eb880ced58d8b0928ac4c0cf87df24032
lbanasik <lukbanasik@gmail.com>
2018-09-15 23:10:02 +0200

    Add bootstrap and both about me and work navs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { WorkComponent } from './components/work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
